725b616558493ece8485f5ec0de3f286d132eba7
iampearlep <damilolaprincess4@gmail.com>
2023-12-31 00:54:54 +0100

    Initial commit

---
+++ app/(pages)/cart/page.tsx
@@ -0,0 +1,6 @@
+export default function Page() {
+    return (
+      <main className='text-black'>Welcome to cart</main>
+    )
+  }
+  
---
+++ app/(pages)/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <main className='text-black'>Hello</main>
+  )
+}
