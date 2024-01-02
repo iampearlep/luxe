fffe26348d95e9beed35de5608876d8053475e52
iampearlep <damilolaprincess4@gmail.com>
2024-01-03 00:04:51 +0100

    feat: add categories

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,10 +1,12 @@
 import Banner from "@/components/Banner"
 import Newarrivals from "@/components/Newarrivals"
+import Categories from "@/components/Categories"
 export default function Home() {
   return (
     <main className=''>
       <Banner />
       <Newarrivals />
+      <Categories />
     </main>
   )
 }
---
+++ components/Categories.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import Img19 from "@/public/images/19.jpg"
+import Img2 from "@/public/images/2.jpg"
+
+const Categories = () => {
+  return (
+    <div className="py-4">
+        <h2 className=" py-8 text-3xl font-semibold w-11/12 mx-auto">Categories.</h2>
+        <div className="grid grid-cols-2">
+            <div>
+            <Image src={Img2} height={632} width={474} alt="" className="w-full " />
+            </div>
+            <div>
+            <Image src={Img19} height={830} width={474} alt="" className="w-full" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Categories
