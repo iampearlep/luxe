2711eecb88ee69add4e8b186b9040d0acf969d17
iampearlep <damilolaprincess4@gmail.com>
2024-01-05 14:35:14 +0100

    fix: fixed footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,17 +7,17 @@ const Footer = () => {
   return (
     <div className="pt-24 pb-20">
       <div className="w-full md:w-11/12 px-5 mx-auto flex flex-col md:flex-row items-start justify-between">
-       <div className="w-full md:w-7/12 grid grid-cols-2 md:flex md:flex-row gap-y-4 items-center md:items-start justify-between">
+       <div className="w-full md:w-7/12 grid grid-cols-1 md:flex md:flex-row gap-y-4 items-center md:items-start justify-between">
        <div className="text-2xl font-extrabold">
             LUXE.
           </div>
           <div>
-          <Link href="/" className="cursor-pointer">
+          <Link href="/" className="cursor-pointer font-semibold text-lg">
             Home
           </Link>
           </div>
         <div className="flex flex-col text-sm gap-y-3 md:gap-y-5">
-          <Link href="/" className="cursor-pointer">
+          <Link href="/" className="cursor-pointer font-semibold text-lg">
            Shop
           </Link>
           <Link href="/" className="cursor-pointer">
@@ -34,7 +34,7 @@ const Footer = () => {
           </Link>
         </div>
         <div className="flex flex-col text-sm gap-y-3 md:gap-y-5">
-          <Link href="/" className="cursor-pointer">
+          <Link href="/" className="cursor-pointer font-semibold text-lg">
             Blog
           </Link>
           <Link href="/" className="cursor-pointer">
@@ -51,7 +51,7 @@ const Footer = () => {
           </Link>
         </div>
         <div className="flex flex-col text-sm gap-y-3 md:gap-y-5">
-          <Link href="/" className="cursor-pointer">
+          <Link href="/" className="cursor-pointer font-semibold text-lg">
             Portfolio
           </Link>
           <Link href="/" className="cursor-pointer">
