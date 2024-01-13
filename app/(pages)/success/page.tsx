862b3b3368d29eb0ace2be5e4fdf3155d7d21f1a
iampearlep <damilolaprincess4@gmail.com>
2024-01-13 13:31:08 +0100

    feat: update success

--- app/(pages)/success/page.tsx
+++ app/(pages)/success/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
     return (
       <main className='text-black text-center'>
-       <h1 className="text-3xl font-semibold">Success!!!</h1>
+       <h1 className="text-3xl font-semibold">Rich Kid!!!</h1>
         <p>Payment successful</p>
       </main>
     )
